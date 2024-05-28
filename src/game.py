1eff6745d390af5a048f4a8b4a559229f22409f5
Mette Munne <mette.ruusu@gmail.com>
2024-05-28 15:27:06 +0300

    add possibility to save strings to trie and machine choice to be based on how often a letter appears after two specific letters

--- src/game.py
+++ src/game.py
@@ -1,4 +1,5 @@
 import random
+from trie import Trie
 
 class RockPaperScissors:
     """Class which runs the game rock-paper-scissors.
@@ -9,6 +10,8 @@ class RockPaperScissors:
         player_points: Number of player wins.
         ai_points: Number of machine/ai wins.
         draw: Number of draws.
+        last_seven: String for storing the last seven of players choices.
+        trie: Data structure for saving strings and substrings.
     """
     def __init__(self):
         """The constructor of the class the RockPaperScissors that sets up the game.
@@ -18,6 +21,8 @@ class RockPaperScissors:
         self.player_points = 0
         self.ai_points = 0
         self.draw = 0
+        self.last_seven = ""
+        self.trie = Trie()
 
     def start(self):
         """Function for starting the game and handling ending display of the game.
@@ -43,21 +48,54 @@ class RockPaperScissors:
         result = self.who_won(players_choice, ai_choice)
         print("Pelikone valitsi", ai_choice)
         print(result)
+        self.create_string(players_choice)
+        self.save_player_choice()
         return True
 
     def get_ai_choice(self):
-        """Placeholder function for getting machines choice to play. Now only randomised.
+        """Test version of function for getting machines choice to play. Mix of randomization and using frequency for strings which have length of two.
         """
 
-        x = random.randint(1, 3)
-        if x == 1:
-            ai_choice = "k"
-        elif x == 2:
-            ai_choice = "p"
-        elif x == 3:
-            ai_choice = "s"
-        return ai_choice
+        if len(self.last_seven) < 2:
+            x = random.randint(1, 3)
+            if x == 1:
+                ai_choice = "k"
+            elif x == 2:
+                ai_choice = "p"
+            elif x == 3:
+                ai_choice = "s"
+            return ai_choice
+        else:
+            max_frequency = self.get_max_frequency_for_two()
+            if max_frequency == "k":
+                ai_choice = "p"
+            elif max_frequency == "p":
+                ai_choice = "s"
+            elif max_frequency == "s":
+                ai_choice = "k"
+            else:
+                x = random.randint(1, 3)
+                if x == 1:
+                    ai_choice = "k"
+                elif x == 2:
+                    ai_choice = "p"
+                elif x == 3:
+                    ai_choice = "s"
+                return ai_choice
+            return ai_choice
     
+    def get_max_frequency_for_two(self):
+        """Function for getting the next max frequency for strings which have the length of two.
+        """
+
+        if len(self.last_seven) < 2:
+            return ""
+        frequencies = self.trie.get_next_frequencies(self.last_seven[-2:])
+        if frequencies:
+            most_frequent_player_move = max(frequencies, key=lambda k: frequencies.get(k))
+            return most_frequent_player_move
+        return ""
+        
     def get_player_choice(self):
         """Function for getting players choice to play.
         """
@@ -70,6 +108,28 @@ class RockPaperScissors:
                 print("Syötä vain kirjain k, p tai s")
 
         return players_choice
+    
+    def create_string(self, players_choice):
+        """Function for updatings string last_seven.
+
+        Args:
+            players_choice (String): Players choice for round.
+        """
+
+        if len(self.last_seven) < 7:
+            self.last_seven += players_choice
+        else:
+            self.last_seven = self.last_seven[1:] + players_choice
+    
+    def save_player_choice(self):
+        """Function for saving every substring of the string to trie if string is at least the lenght of 2 but not longer than 7.
+        """
+
+        if len(self.last_seven) >= 2:
+            for length in range(2, len(self.last_seven) + 1):
+                substring = self.last_seven[-length:]
+                self.trie.add(substring)
+        
 
     def who_won(self, players_choice, ai_choice):
         """Function for figuring out who won the round.
