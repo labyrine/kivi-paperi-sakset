e2ef7ddd7886b1fb3aed6d465704c49395770177
Mette Munne <mette.ruusu@gmail.com>
2024-09-01 15:27:05 +0300

    add more unit test for ai

--- src/tests/ai_test.py
+++ src/tests/ai_test.py
@@ -1,3 +1,4 @@
+import random
 import unittest
 from unittest.mock import patch, MagicMock
 from ai import BaseAi, AiSelector
@@ -37,6 +38,9 @@ class TestBaseAi(unittest.TestCase):
         self.assertEqual(ai.counter_move("p"), "s")
         self.assertEqual(ai.counter_move("s"), "k")
 
+        with self.assertRaises(ValueError) as context:
+            ai.counter_move("x")
+        self.assertEqual(str(context.exception), "Virheellinen valinta: x")
 
 class TestAiSelector(unittest.TestCase):
     def setUp(self):
@@ -57,7 +61,7 @@ class TestAiSelector(unittest.TestCase):
             self.assertEqual(model.last_seven, self.last_seven1)
 
     @patch('ai.BaseAi')
-    def test_update_scores_p(self, MockBaseAi):
+    def test_update_scores_kps(self, MockBaseAi):
         mock_base_ai_instance = MagicMock()
         mock_base_ai_instance.predict_ai_choice.side_effect = [
             "k", "p", "s", "k", "p", "s"]
@@ -75,6 +79,23 @@ class TestAiSelector(unittest.TestCase):
         self.assertEqual(ai_selector.focus_length_stats, valid_stats)
 
     @patch('ai.BaseAi')
+    def test_update_scores_only_s(self, MockBaseAi):
+        mock_base_ai_instance = MagicMock()
+        mock_base_ai_instance.predict_ai_choice.side_effect = [
+            "s", "s", "s", "s", "s", "s"]
+        MockBaseAi.side_effect = [mock_base_ai_instance for _ in range(6)]
+        
+        ai_selector = AiSelector(5, self.trie_mock)
+        ai_selector.update_last_seven("kpppssp")
+        ai_selector.update_scores()
+
+        valid_scores = [1, 1, 1, 1, 1, 1]
+        valid_stats = [[[1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0]]]
+
+        self.assertEqual(ai_selector.focus_length_scores[0], valid_scores)
+        self.assertEqual(ai_selector.focus_length_stats, valid_stats)
+
+    @patch('ai.BaseAi')
     def test_select_best_ai_valid(self, MockBaseAi):
         mock_base_ai_instance = MagicMock()
         MockBaseAi.side_effect = [mock_base_ai_instance for _ in range(6)]
@@ -86,3 +107,29 @@ class TestAiSelector(unittest.TestCase):
 
         ai_selector.select_best_ai()
         self.assertEqual(ai_selector.best_model, ai_selector.models[5])
+
+    @patch('ai.BaseAi')
+    def test_play_ai_with_prediction(self, MockBaseAi):
+        mock_base_ai_instance = MagicMock()
+        mock_base_ai_instance.predict_ai_choice.return_value = "p"
+        MockBaseAi.return_value = mock_base_ai_instance
+    
+        ai_selector = AiSelector(5, self.trie_mock)
+        ai_selector.best_model = mock_base_ai_instance
+        ai_choice = ai_selector.play_ai()
+    
+        self.assertEqual(ai_choice, "p")
+
+    @patch('ai.BaseAi')
+    @patch('random.choice')
+    def test_play_ai_with_random_choice(self, mock_random_choice, MockBaseAi):
+        mock_base_ai_instance = MagicMock()
+        mock_base_ai_instance.predict_ai_choice.return_value = ""
+        MockBaseAi.return_value = mock_base_ai_instance
+        mock_random_choice.return_value = "k"
+
+        ai_selector = AiSelector(5, self.trie_mock)
+        ai_selector.best_model = mock_base_ai_instance
+        ai_choice = ai_selector.play_ai()
+
+        self.assertEqual(ai_choice, "k")
